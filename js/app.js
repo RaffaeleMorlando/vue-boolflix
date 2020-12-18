ba03a8a3d82e1484b8fe7cbba514bb77c4037210
Raffaele <morlandora@gmail.com>
2020-12-18 16:15:16 +0100

    First Commit

---
+++ js/app.js
@@ -0,0 +1,38 @@
+// API-KEY
+// https://api.themoviedb.org/3/movie/550?api_key=235a6ac442ac4f61d5783c4fbcc2b7ae
+
+
+
+const app = new Vue(
+  {
+    el: '#app',
+    data: {
+      searchValue: '',
+      moviesList: []
+    },
+    methods: {
+      getMovies: function() {
+        const self = this;
+        axios
+        .get('https://api.themoviedb.org/3/movie/550?', {
+          params: {
+            api_key: '235a6ac442ac4f61d5783c4fbcc2b7ae',
+            query: this.searchValue,
+            language: 'it-IT'
+          }
+        })
+        .then(function(result) {
+          self.moviesList = [];
+          self.moviesList.push(result.data);
+          console.log(result.data);
+        })
+      }
+    }
+  }
+);
+
+// const card = document.querySelector('.movie-inner-card');
+// card.addEventListener('click', function() {
+//   card.classList.toggle('is-flipped')
+//   console.log('is flipped');
+// });
