ab0589d94f69d2e0aacd94ff78f313e173b2b696
Raffaele <morlandora@gmail.com>
2020-12-21 15:12:05 +0100

    add cast

--- js/app.js
+++ js/app.js
@@ -10,43 +10,18 @@ const app = new Vue(
       popularUrl: 'https://api.themoviedb.org/3/trending/all/day',
       prefixUrl: 'https://image.tmdb.org/t/p/w600_and_h900_bestv2',
       urlImg:'img/imgflags/',
-      extensionImg: '.svg'
+      extensionImg: '.svg',
+      generesId: [],
+      cast:[]
     },
     mounted(){
       this.getPopular();
     },
     methods: {
       getSearched: function() {
-        if(this.searchValue.length != 0) {
-          // movies
-          axios
-          .get(this.movieUrl, {
-            params: {
-              api_key: this.api_key,
-              query: this.searchValue,
-              language: 'en-EN',
-            }
-          })
-          .then((result) =>  {
-            this.searchedList = [];
-            this.searchedList = result.data.results
-            console.log(result.data.results);
-          })
-           // tv shows
-          axios
-          .get(this.tvShowUrl, {
-            params: {
-              api_key: this.api_key,
-              query: this.searchValue,
-              language: 'en-EN',
-            }
-          })
-          .then((result) =>  {
-            this.searchedList = [...result.data.results];
-          })
-        } else {
-          this.searchedList = this.getPopular();
-        }
+        this.getMovies();
+        this.getTvShow();
+
       },
       // show all daily trendings tv shows / movies 
       getPopular: function() {
@@ -62,6 +37,77 @@ const app = new Vue(
         .then((result) => {
           this.searchedList = result.data.results;
         })
+      },
+      getMovies: function() {
+        if(this.searchValue.length != 0) {
+          axios
+          .get(this.movieUrl, {
+            params: {
+              api_key: this.api_key,
+              query: this.searchValue,
+              language: 'en-EN',
+            }
+          })
+          .then((result) =>  {
+            this.searchedList = [];
+            this.searchedList = result.data.results
+            console.log(result.data.results);
+          })
+        } else {
+            this.searchedList = this.getPopular();
+        }
+      },
+      getTvShow: function() {
+        if(this.searchValue.length != 0) {
+          axios
+          .get(this.tvShowUrl, {
+            params: {
+              api_key: this.api_key,
+              query: this.searchValue,
+              language: 'en-EN',
+            }
+          })
+          .then((result) =>  {
+            this.searchedList = [...result.data.results];
+            console.log(result.data.results);
+          })
+        } else {
+            this.searchedList = this.getPopular();
+        }
+      },
+      getCast: function(movie) {
+        
+        if(movie.title) {
+          axios
+          .get(`https://api.themoviedb.org/3/movie/${movie.id}/credits`, {
+            params: {
+              api_key: this.api_key,
+              language: 'en-EN',
+            }
+          })
+          .then((result) => {  
+            this.cast = [];
+            const castArray = result.data.cast;
+            for(let i = 0; i < 4; i++) {
+              this.cast.push(castArray[i].name);
+            }
+          })
+        } else {
+          axios
+          .get(`https://api.themoviedb.org/3/tv/${movie.id}/credits`, {
+            params: {
+              api_key: this.api_key,
+              language: 'en-EN',
+            }
+          })
+          .then((result) => {  
+            this.cast = [];
+            const castArray = result.data.cast;
+            for(let i = 0; i < 4; i++) {
+              this.cast.push(castArray[i].name);
+            }
+          })
+        }
       }
     },
     filters: {
@@ -71,3 +117,72 @@ const app = new Vue(
     }
   }
 );
+
+
+
+
+
+ // getInfo: function(index) {
+      //   console.log(this.searchedList[index].id);
+      //   // this.getCredits(infoId);s
+      // },
+      // getCredits: function() {
+      // }
+      // getGeneres: function() {
+      //   // ************************
+      //   const self = this;
+      //   axios
+      //   .get('https://api.themoviedb.org/3/genre/tv/list', {
+      //     params: {
+      //       api_key: this.api_key,
+      //       language: 'en-EN',
+      //     }
+      //   })
+      //   .then((result) =>  {
+      //     // array con generi 
+      //     self.generesId = result.data;
+      //     const newArray = self.searchedList.map(
+      //       (item) => {
+      //         console.log(item);
+      //        return item.genre_ids.forEach(
+      //           (genres) => {
+      //             self.generesId.forEach(
+      //             (element) => {
+      //             genres.includes(element).item.push(element)
+                  
+      //           });
+      //         });
+                
+      //       }
+      //     )
+      //     console.log(newArray);
+      //   })
+        
+      // }
+
+
+
+
+       // getId: function() {
+      //   const arrayId = this.searchedList.map(
+      //     (movie) => {
+      //       return movie.id
+      //     }
+      //   )
+      // }
+      // ,
+      // getCast: function(arrayId) {
+      //   arrayId.forEach(
+      //     (id) => {
+      //       axios
+      //     .get(`https://api.themoviedb.org/3/movie/${id}/credits`, {
+      //       api_key: this.api_key,
+      //       language: 'en-EN'
+      //     })
+      //     .then((result) => {
+      //       console.log(result);
+      //     })
+      //   }
+      //   );
+        
+      // }
